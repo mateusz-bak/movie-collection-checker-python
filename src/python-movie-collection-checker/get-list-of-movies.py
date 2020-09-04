88c8f832d3e9494b2a176c6887f4d498547832fd
mateusz <mateusz.bak@protonmail.com>
2020-09-04 15:27:16 +0100

    Added environmental variables for handling user data.

--- src/python-movie-collection-checker/get-list-of-movies.py
+++ src/python-movie-collection-checker/get-list-of-movies.py
@@ -1,9 +1,7 @@
 import os
 import requests
 import json
-import config
 import datetime
-# from extract import json_extract
 
 
 def cls():
@@ -20,7 +18,6 @@ def get_list_of_movies_from_radarr(instance, api_key):
 
     for x in range(0, len(data)):
         buffer = data[x]["tmdbId"]
-        # print(buffer)
         list_of_ids.append(buffer)
 
     return list_of_ids
@@ -37,7 +34,6 @@ def get_movie_details_from_tmdb(movie_id, api_key):
 def get_collection_details_from_tmdb(collection_id, api_key):
     api_address = "https://api.themoviedb.org/3/collection/"
     api_call = api_address + collection_id + "?api_key=" + api_key
-    # print(api_call)
     response = requests.get(api_call)
     data = response.json()
     return data
@@ -61,8 +57,6 @@ def check_name_of_collection(movie_id, tmdb_api_key):
         return 0
     else:
         collection_name = movie_details['belongs_to_collection']['name']
-        # title = movie_details['original_title']
-        # detail = title + " belongs to collection: " + collection
         return collection_name
 
 
@@ -72,19 +66,15 @@ def check_id_of_collection(movie_id, tmdb_api_key):
         return 0
     else:
         collection_id = movie_details['belongs_to_collection']['id']
-        # title = movie_details['original_title']
-        # detail = title + " belongs to collection: " + collection
         return collection_id
 
 
 def make_a_list_of_owned_collections(data, tmdb_api_key):
-    # list_of_movies_ids = json_extract(data, "tmdbId")
     list_of_collections = []
     counter = 0
     number_of_movies = len(data)
 
     for x in data:
-        # print(x)
         # wrong ID for a in TMDB movie - needs to be adjusted here
         if x == 690882:
             collection_id = check_id_of_collection("664847",
@@ -94,18 +84,11 @@ def make_a_list_of_owned_collections(data, tmdb_api_key):
                                                    tmdb_api_key)
         counter += 1
 
-        # next 3 lines for testing
-        # if counter == 100:
-        #     print("100 movies, bailing out...")
-        #     break
-
-        # print(str(counter))
         if collection_id == 0:
             pass
         else:
             if collection_id not in list_of_collections:
                 list_of_collections.append(collection_id)
-                cls()
                 print("Movies: " + str(counter) + "/" + str(number_of_movies))
                 print("Found " + str(len(list_of_collections)) +
                       " collections")
@@ -119,7 +102,6 @@ def get_movies_id_in_a_collection(collection, api_key):
 
     for x in range(0, len(collection_details_parts)):
         collection_details_parts_titles = collection_details_parts[x]["id"]
-        # print(collection_details_parts_titles)
         movies_ids_in_collection.append(collection_details_parts_titles)
     return movies_ids_in_collection
 
@@ -128,45 +110,27 @@ def check_if_movie_exists_in_library(movies, movies_in_collection, api_key):
     for x in movies_in_collection:
         if x in movies:
             pass
-            # print(str(x) + " found")
         else:
-            # print(str(x) + " not found")
             movie_details = get_movie_details_from_tmdb(str(x), api_key)
 
             release_date = movie_details["release_date"]
-            # print("-----")
             movie_title = movie_details["title"]
-            # print(movie_title)
-            # print(release_date)
-            # print(release_date)
             release_date = release_date.split("-")
-            # print(release_date)
 
             if release_date == [""]:
-                # print("Release date is empty for this movie")
                 result = movie_title + " ( )"
-                # print(result)
-                # store_results_in_txt(result,
-                #                      'missing-movies-no-release-date.txt')
                 pass
             else:
                 d1 = datetime.datetime(int(release_date[0]),
                                        int(release_date[1]),
                                        int(release_date[2])).date()
                 d1_year = d1.year
-                # print(d1)
-                # print(d1_year)
                 d2 = datetime.datetime.now().date()
-                # print(d2)
                 if d1 < d2:
-                    # print("Movie is released.")
-                    # print(str(d1) + " > " + str(d2))
-                    # movie_title = movie_details["title"]
                     result = movie_title + " (" + str(d1_year) + ")"
                     print(result)
-                    # store_results_in_txt(result, 'missing-movies.txt')
+                    store_results_in_txt(result, 'output/missing-movies.txt')
                 else:
-                    # print("Movie is not released.")
                     pass
 
 
@@ -176,19 +140,37 @@ def store_results_in_txt(result, filename):
     f.close()
 
 
+def set_radarr_instance():
+    protocol = os.environ.get('RADARR_PROTOCOL')
+    ip = os.environ.get('radarr_ip')
+    port = os.environ.get('radarr_port')
+
+    url = protocol + "://" + ip + ":" + port + "/"
+    return url
+
+
+def set_radarr_api_key():
+    key = os.environ.get('radarr_api_key')
+    return key
+
+
+def set_tmdb_api_key():
+    key = os.environ.get('tmdb_api_key')
+    return key
+
+
 if __name__ == '__main__':
-    open('missing-movies.txt', 'w').close()
-    # open('missing-movies-no-release-date.txt', 'w').close()
-    movies = get_list_of_movies_from_radarr(config.radarr_instance,
-                                            config.radarr_api_key)
+    radarr_instance = set_radarr_instance()
+    radarr_api_key = set_radarr_api_key()
+    tmdb_api_key = set_tmdb_api_key()
 
-    # print(len(data))
-    collections = make_a_list_of_owned_collections(movies, config.tmdb_api_key)
-    # print(collections)
+    open('output/missing-movies.txt', 'w').close()
 
-    # collection = "9485"
+    movies = get_list_of_movies_from_radarr(radarr_instance, radarr_api_key)
+
+    collections = make_a_list_of_owned_collections(movies, tmdb_api_key)
+
     for x in collections:
-        movies_in_coll = get_movies_id_in_a_collection(str(x),
-                                                       config.tmdb_api_key)
-        check_if_movie_exists_in_library(movies, movies_in_coll,
-                                         config.tmdb_api_key)
+        movies_in_coll = get_movies_id_in_a_collection(str(x), tmdb_api_key)
+        check_if_movie_exists_in_library(movies, movies_in_coll, tmdb_api_key)
+    print("Wrote list of missing movies to output/missing-movies.txt.")
