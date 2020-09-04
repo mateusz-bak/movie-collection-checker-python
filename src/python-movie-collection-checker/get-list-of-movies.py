3e843f12bd9cafe138e475a8ff0c6705ae331aa3
mateusz <mateusz.bak@protonmail.com>
2020-09-04 10:15:51 +0100

    Script returns a txt file with missing movies.

--- src/python-movie-collection-checker/get-list-of-movies.py
+++ src/python-movie-collection-checker/get-list-of-movies.py
@@ -2,7 +2,8 @@ import os
 import requests
 import json
 import config
-from extract import json_extract
+import datetime
+# from extract import json_extract
 
 
 def cls():
@@ -77,22 +78,27 @@ def check_id_of_collection(movie_id, tmdb_api_key):
 
 
 def make_a_list_of_owned_collections(data, tmdb_api_key):
-    list_of_movies_ids = json_extract(data, "tmdbId")
+    # list_of_movies_ids = json_extract(data, "tmdbId")
     list_of_collections = []
     counter = 0
-    number_of_movies = len(list_of_movies_ids)
+    number_of_movies = len(data)
 
-    for x in list_of_movies_ids:
+    for x in data:
         # print(x)
-
         # wrong ID for a in TMDB movie - needs to be adjusted here
         if x == 690882:
             collection_id = check_id_of_collection("664847",
-                                                             tmdb_api_key)
+                                                   tmdb_api_key)
         else:
             collection_id = check_id_of_collection(str(x),
-                                                             tmdb_api_key)
+                                                   tmdb_api_key)
         counter += 1
+
+        # next 3 lines for testing
+        # if counter == 100:
+        #     print("100 movies, bailing out...")
+        #     break
+
         # print(str(counter))
         if collection_id == 0:
             pass
@@ -100,8 +106,9 @@ def make_a_list_of_owned_collections(data, tmdb_api_key):
             if collection_id not in list_of_collections:
                 list_of_collections.append(collection_id)
                 cls()
-                print("Movies: " +str(counter) + "/" + str(number_of_movies))
-                print("Found " + str(len(list_of_collections)) + " collections")
+                print("Movies: " + str(counter) + "/" + str(number_of_movies))
+                print("Found " + str(len(list_of_collections)) +
+                      " collections")
     return list_of_collections
 
 
@@ -125,19 +132,45 @@ def check_if_movie_exists_in_library(movies, movies_in_collection, api_key):
         else:
             # print(str(x) + " not found")
             movie_details = get_movie_details_from_tmdb(str(x), api_key)
-            print(movie_details["title"])
 
+            release_date = movie_details["release_date"]
+            # print(release_date)
+            release_date = release_date.split("-")
+            print(release_date)
+            d1 = datetime.datetime(int(release_date[0]), int(release_date[1]),
+                                   int(release_date[2])).date()
+            d1_year = d1.year
+            # print(d1)
+            # print(d1_year)
+            d2 = datetime.datetime.now().date()
+            # print(d2)
+
+            if d1 < d2:
+                # print(str(d1) + " > " + str(d2))
+                movie_title = movie_details["title"]
+                result = movie_title + " (" + str(d1_year) + ")"
+                print(result)
+                store_results_in_txt(result)
+
+
+def store_results_in_txt(result):
+    f = open("missing-movies.txt", "a")
+    f.write(result + "\n")
+    f.close()
 
 
 if __name__ == '__main__':
+    open('missing-movies.txt', 'w').close()
     movies = get_list_of_movies_from_radarr(config.radarr_instance,
-                                          config.radarr_api_key)
+                                            config.radarr_api_key)
 
     # print(len(data))
-    # collections = make_a_list_of_owned_collections(data, config.tmdb_api_key)
+    collections = make_a_list_of_owned_collections(movies, config.tmdb_api_key)
     # print(collections)
 
-    collection = "9485"
-    movies_in_collection = get_movies_id_in_a_collection(collection, config.tmdb_api_key)
-
-    check_if_movie_exists_in_library(movies, movies_in_collection, config.tmdb_api_key)
+    # collection = "9485"
+    for x in collections:
+        movies_in_coll = get_movies_id_in_a_collection(str(x),
+                                                       config.tmdb_api_key)
+        check_if_movie_exists_in_library(movies, movies_in_coll,
+                                         config.tmdb_api_key)
